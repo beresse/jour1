0d99b5d19130b52abca31d21882b8c1be14807e1
Edmond <Edmondwilmore1er@gmail.com>
2021-02-24 01:19:55 +0100

    un push qui fonctionne

--- App.js
+++ App.js
@@ -1,21 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, ScrollView, Image, ActivityIndicator, Pressable,Alert } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={styles.containerUn}>
+      <View style={styles.containerDeux}>
+        <Text style={styles.textUn}>Premier texte </Text>
+        <Text style={styles.textDeux}>Second texte</Text>
+        <Text style={styles.textTrois}>Troisième texte</Text>
+      </View>
+
+      <View style={styles.containerLoko}>
+        <Image
+          style={styles.loko}
+          source={{
+            uri:
+              "https://www.konexio.eu/uploads/1/2/0/2/120245745/konexio-logo_1.png",
+          }}
+        ></Image>
+        <Image
+          style={styles.loko}
+          source={require("./img/konexio.png")}
+        ></Image>
+        
+      </View>
+      
+     <Pressable onPress={() => alert(`Buy it, use it, break it, fix it, trash it, change it, mail, upgrade it
+Charge it, point it, zoom it, press it, snap it, work it, quick erase it
+Write it, cut it, paste it, save it, load it, check it, quick rewrite it
+Plug it, play it, burn it, rip it, drag it, drop it, zip`)}>
+        <Text>Click Me</Text>
+      </Pressable>
+      <ActivityIndicator size="large" color="red" />
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  containerUn: {
+    backgroundColor: "blue",
   },
+  containerDeux: {
+    backgroundColor: "white",
+  },
+  textUn: {
+    marginVertical: 80,
+    fontSize: 30,
+  },
+  textDeux: {
+    marginVertical: 80,
+    textAlign: "center",
+  },
+  textTrois: {
+    marginVertical: 80,
+    fontWeight: "bold",
+  },
+  containerLoko:{
+    alignItems: "center",
+  
+  },
+  loko: {
+    height: 40,
+    width: 124,
+    marginVertical: 16,
+  }
 });
